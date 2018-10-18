ab1010c08606fc196799b5a7789730dadf923359
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2018-10-18 16:09:25 +0800

    feat: Setup react-dom to html and add StorePicker

---
+++ src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render(){
+        return <p>I am a StorePicker component</p>
+    };
+};
+
+export default StorePicker;
