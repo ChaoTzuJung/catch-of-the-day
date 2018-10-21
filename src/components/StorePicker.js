2ec81cad90062c21e314b460d5eefc9f4936135b
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2018-10-21 17:28:15 +0800

    fix: Use ref to get input dom

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
-    render(){
+
+    goToStore(event) {
+        // Stop form from sumbitting
+        event.preventDefault();
+        // Get the text from input
+        console.log(this) // undefined 
+        // Change the page to /store/whatever-they-enter
+        this.componentDidMount() {
+            console.log(this) // StorePicker這個component
+        }
+    }
+    render() {
         return (
             <React.Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubnit={this.goToStore}>
                     <h2>Please Enter a Store</h2>
                     <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
                     <button type="submit" >Visit Store</button>
