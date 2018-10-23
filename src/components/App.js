e2647447eb1abbfed6757af8a270ef7e2f76dd22
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2018-10-24 01:16:20 +0800

    feat: Display order state and show price which has been counted

--- src/components/App.js
+++ src/components/App.js
@@ -50,7 +50,8 @@ class App extends React.Component {
                         )}
                     </ul>
                 </div>
-                <Order />
+                { /* <Order fishes={this.state.fishes} order={this.state.order} /> */ }
+                <Order {...this.state} />
                 <Inventory addFish={this.addFish} loadSampleFish={this.loadSampleFish}/>
             </div>
         )
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import { formatPrice } from '../helpers';
 class Order extends React.Component {
-    render(){
+    render() {
+        const orderIds = Object.keys(this.props.order);
+        const total = orderIds.reduce((prevTotal, key) => {
+            const fish = this.props.fishes[key]; // 清單內有哪幾種魚
+            const count = this.props.order[key]; // 清單每種魚的數量
+            const isAvailable = fish && fish.status === 'available';
+            if(isAvailable) {
+                // 每次都累加價格
+                return prevTotal + (count * fish.price)
+            }
+            return prevTotal
+        }, 0)
         return (
-            <div className="order">
-                Order!!!
+            <div className="order-wrap">
+                <h2>Order</h2>
+                <ul>
+                    {orderIds.map(key => <li>{key}</li>)}
+                </ul>
+                
+                <div className="total">
+                    Total: 
+                    <strong>{formatPrice(total)}</strong>
+                </div>
             </div>
-        )
+        );
     };
 };
 
