db7d6daa80c474bd83362417e7c5ad63ecaa8717
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2018-10-23 14:04:17 +0800

    feat: Add fish list to menu as click load button

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish'
 class App extends React.Component {
     state = {
         fishes: {},
@@ -26,6 +27,9 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order />
                 <Inventory addFish={this.addFish} loadSampleFish={this.loadSampleFish}/>
